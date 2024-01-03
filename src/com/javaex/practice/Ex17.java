ade9af98067821f6c24bb5ec61ecd1442f2c8636
rhs0402 <rhs0402@naver.com>
2024-01-03 18:04:42 +0900

    2024-01-03(수)

--- src/com/javaex/practice/Ex17.java
+++ src/com/javaex/practice/Ex17.java
@@ -2,25 +2,23 @@ package com.javaex.practice;
 
 import java.util.Scanner;
 
-public class Ex16 {
+public class Ex17 {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
 		System.out.print("숫자를 입력하세요: ");
 		int num = sc.nextInt();
-		int s = 0;
-		int sum=0;
 		
-		for(int i=5; i<=num; i=i+5) {
-			s++;
-			sum=sum+i;	
+		for( int i=1; i<=num; i++) {
+			for(int z=i; z<num; z++) {
+				System.out.print("*");
+			}
+			System.out.println("*");
 		}
-		System.out.println("5의배수의 개수: "+ s);
-		System.out.println("5의배수의 합 : " + sum);
 		
 		sc.close();
+		
 	}
 
 }
